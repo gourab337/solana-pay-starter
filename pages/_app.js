721f5502cc62adad649bc7762c97b2eb7f0baacb
gourab337 <19bcs118@iiitdwd.ac.in>
2023-01-09 19:39:57 +0530

    Full build

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,40 @@
 import React, { useMemo } from "react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { GlowWalletAdapter, PhantomWalletAdapter, SlopeWalletAdapter, SlopeWalletName, SolflareWalletAdapter, TorusWalletAdapter, } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import "@solana/wallet-adapter-wallets";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
 import "../styles/globals.css";
 import "../styles/App.css";
 
 const App = ({ Component, pageProps }) => {
+  // Set to 'devnet', 'testnet' or 'mainnet-beta'
+
+  const network = WalletAdapterNetwork.Devnet;
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]); 
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new TorusWalletAdapter(),
+    ],
+    [network]
+  );
 
   return (
-    <Component {...pageProps} />
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 };
 
